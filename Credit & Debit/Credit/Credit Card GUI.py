41233bf360c0428be5bd755b0efabf4621a3aa80
Alaa19971234 <95756868+Alaa19971234@users.noreply.github.com>
2022-03-15 21:52:30 -0400

    Update Credit Card GUI.py

--- Credit & Debit/Credit/Credit Card GUI.py
+++ Credit & Debit/Credit/Credit Card GUI.py
@@ -16,32 +16,38 @@ class SampleApp(tk.Tk):
         self.button.pack()
         self.UserInput.pack()
         
+    
     def validator(self):
 #checks if the user's input follows the credit card algorithm 
-        validatelist=[]
-
-        for i in self.UserInput.get():
-            validatelist.append(int(i))
-
-
-        for i in range(0,len(self.UserInput.get()),2):
-
-
-            validatelist[i] = validatelist[i]*2
-
-            if validatelist[i] >= 10:
-
-                validatelist[i] = validatelist[i]//10 + validatelist[i]%10
-
-
-        if sum(validatelist)%10 == 0:
-            self.valid = tk.Label(self, text="This a valid credit card")
-            self.valid.pack()
-            #print('This a valid credit card') 
-
+        if not (len(self.UserInput.get()) == 16) or not type(int(self.UserInput.get()) == int) :
+            self.wrong = tk.Label(self, text="That is not a proper credit card number. \nMake sure you are entering digits not characters and all the 16 digits.")
+            self.wrong.pack()
         else:
-            self.invalid = tk.Label(self, text="This is not valid credit card")
-            self.invalid.pack()          
+            validatelist=[]
+        
+        
+            for i in self.UserInput.get():
+                validatelist.append(int(i))
+
+
+            for i in range(0,len(self.UserInput.get()),2):
+
+
+                validatelist[i] = validatelist[i]*2
+
+                if validatelist[i] >= 10:
+
+                    validatelist[i] = validatelist[i]//10 + validatelist[i]%10
+
+        
+            if sum(validatelist)%10 == 0:
+                self.valid = tk.Label(self, text="This a valid credit card")
+                self.valid.pack()
+                #print('This a valid credit card') 
+
+            else:
+                self.invalid = tk.Label(self, text="This is not valid credit card")
+                self.invalid.pack()          
             #print('This is not valid credit card')
         
         
@@ -50,5 +56,3 @@ class SampleApp(tk.Tk):
 
 app = SampleApp()
 app.mainloop()
-
-
