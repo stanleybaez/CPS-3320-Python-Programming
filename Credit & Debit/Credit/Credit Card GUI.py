89c78193f6297afa181c934eafee61f2043133fb
Alaa19971234 <95756868+Alaa19971234@users.noreply.github.com>
2022-03-25 22:10:17 -0400

    Update Credit Card GUI.py

--- Credit & Debit/Credit/Credit Card GUI.py
+++ Credit & Debit/Credit/Credit Card GUI.py
@@ -2,24 +2,28 @@ import tkinter as tk
 
 class SampleApp(tk.Tk):
     def __init__(self):
+        #self.wrong.destroy()
         tk.Tk.__init__(self)
         self.title('Credit Card GUI')
         self.configure(bg='orange')
         self.geometry("300x200")
         self.label = tk.Label(self, text="Please enter the 16 digit credit card number : ", bg='orange')
         self.UserInput = tk.Entry(self, show = '*', bg='orange')
-        self.button = tk.Button(self, text="Confirm", command=self.validator, bg='orange')
+        self.confirm = tk.Button(self, text="Confirm", command=self.validator, bg='orange')
         self.label.pack()
-        self.button.pack()
+        self.confirm.pack()
         self.UserInput.pack()
-        
-    
+            
     def validator(self):
-#checks if the user's input follows the credit card algorithm 
+
         if not (len(self.UserInput.get()) == 16) or not type(int(self.UserInput.get()) == int) :
-            self.wrong = tk.Label(self, text="That is not a proper credit card number. \nMake sure you are entering digits not characters and all the 16 digits.")
+            self.retry = tk.Button(self, text="Try again?", command=self.Retry, bg='orange')
+            self.retry.pack()
+            self.wrong = tk.Label(self, text="That is not a proper credit card number. \nMake sure you are entering digits not characters and all the 16 digits.", bg='orange')
             self.wrong.pack()
+            self.confirm.destroy()
         else:
+            #self.wrong.destroy()
             validatelist=[]
         
         
@@ -38,19 +42,29 @@ class SampleApp(tk.Tk):
 
         
             if sum(validatelist)%10 == 0:
-                self.valid = tk.Label(self, text="This a valid credit card")
+                self.valid = tk.Label(self, text="This a valid credit card", bg='orange')
                 self.valid.pack()
                 #print('This a valid credit card') 
 
             else:
-                self.invalid = tk.Label(self, text="This is not valid credit card")
-                self.invalid.pack()          
+                self.retry2 = tk.Button(self, text="Try again?", command=self.Retry2, bg='orange')
+                self.retry2.pack()
+                self.invalid = tk.Label(self, text="This is not valid credit card", bg='orange')
+                self.invalid.pack()
+                self.confirm.destroy()
             #print('This is not valid credit card')
         
+    def Retry2(self):
+        self.invalid.destroy()
+        self.retry2.destroy()
+        self.confirm = tk.Button(self, text="Confirm", command=self.validator, bg='orange')
+        self.confirm.pack()
         
-    def on_button(self):
-        print(self.UserInput.get())
-
+    def Retry(self):
+        self.wrong.destroy()
+        self.retry.destroy()
+        self.confirm = tk.Button(self, text="Confirm", command=self.validator, bg='orange')
+        self.confirm.pack()
+        #print(self.UserInput.get())        
 app = SampleApp()
 app.mainloop()
-#4642917704285981
