58832f83c8bbd513eac54db55dfe57c73b28f0a0
Alaa19971234 <95756868+Alaa19971234@users.noreply.github.com>
2022-04-02 13:29:59 -0400

    Update Credit Card GUI.py

--- Credit & Debit/Credit/Credit Card GUI.py
+++ Credit & Debit/Credit/Credit Card GUI.py
@@ -1,22 +1,37 @@
 import tkinter as tk
 
 class SampleApp(tk.Tk):
+    
+#GUI setup
     def __init__(self):
-        #self.wrong.destroy()
         tk.Tk.__init__(self)
+        
+        #GUI title
         self.title('Credit/Debit Card')
+        
+        #GUI background color
         self.configure(bg='orange')
+        
+        #GUI size when first launched
         self.geometry("300x200")
+        
+        #the welcome text
         self.label = tk.Label(self, text="Please enter the 16 digit credit card number : ", bg='orange')
+        
+        #input box
         self.UserInput = tk.Entry(self, show = '*', bg='orange')
+        
+        #the confirm button
         self.confirm = tk.Button(self, text="Confirm", command=self.validator, bg='orange')
+        
         self.label.pack()
         self.confirm.pack()
         self.UserInput.pack()
-            
+
+#A function with two conditions to check the user's input to see if they entered a valid card
     def validator(self):
-        #if self.cardType:
-            #self.cardType.destroy()
+        
+        #first condition
         if not (len(self.UserInput.get()) == 16) or not type(int(self.UserInput.get()) == int) :
             self.retry = tk.Button(self, text="Try again?", command=self.Retry, bg='orange')
             self.retry.pack()
@@ -24,7 +39,6 @@ class SampleApp(tk.Tk):
             self.wrong.pack()
             self.confirm.destroy()
         else:
-            #self.wrong.destroy()
             validatelist=[]
         
         
@@ -41,13 +55,13 @@ class SampleApp(tk.Tk):
 
                     validatelist[i] = validatelist[i]//10 + validatelist[i]%10
 
-        
+            #second condition
             if sum(validatelist)%10 == 0:
                 self.valid = tk.Label(self, text="This a valid credit card", bg='orange')
                 self.valid.pack()
                 
                 
-                
+            #Extra conditions that checks the card type 
                 if self.UserInput.get()[0] == "1":
                     self.cardType = tk.Label(self, text="Airlines", bg='orange')
                 if self.UserInput.get()[0] == "2":
@@ -86,14 +100,15 @@ class SampleApp(tk.Tk):
                 self.invalid = tk.Label(self, text="This is not valid credit card", bg='orange')
                 self.invalid.pack()
                 self.confirm.destroy()
-            #print('This is not valid credit card')
         
+        #the try again button if user's input failed to pass the second condition
     def Retry2(self):
         self.invalid.destroy()
         self.retry2.destroy()
         self.confirm = tk.Button(self, text="Confirm", command=self.validator, bg='orange')
         self.confirm.pack()
         
+        #the try again button if user's input failed to pass the first condition
     def Retry(self):
         self.wrong.destroy()
         self.retry.destroy()
@@ -102,6 +117,8 @@ class SampleApp(tk.Tk):
         #print(self.UserInput.get())        
 app = SampleApp()
 app.mainloop()
+
+#valid card 16 digits example
 #4276983748842032
 #6011356599462496
 #3448482730607508
