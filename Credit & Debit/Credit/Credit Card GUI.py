55e1066ddfc30ab23eb304a343920fd37b3e173b
Alaa19971234 <95756868+Alaa19971234@users.noreply.github.com>
2022-04-02 13:12:37 -0400

    Update Credit Card GUI.py

--- Credit & Debit/Credit/Credit Card GUI.py
+++ Credit & Debit/Credit/Credit Card GUI.py
@@ -4,7 +4,7 @@ class SampleApp(tk.Tk):
     def __init__(self):
         #self.wrong.destroy()
         tk.Tk.__init__(self)
-        self.title('Credit Card GUI')
+        self.title('Credit/Debit Card')
         self.configure(bg='orange')
         self.geometry("300x200")
         self.label = tk.Label(self, text="Please enter the 16 digit credit card number : ", bg='orange')
@@ -15,7 +15,8 @@ class SampleApp(tk.Tk):
         self.UserInput.pack()
             
     def validator(self):
-
+        #if self.cardType:
+            #self.cardType.destroy()
         if not (len(self.UserInput.get()) == 16) or not type(int(self.UserInput.get()) == int) :
             self.retry = tk.Button(self, text="Try again?", command=self.Retry, bg='orange')
             self.retry.pack()
@@ -44,8 +45,41 @@ class SampleApp(tk.Tk):
             if sum(validatelist)%10 == 0:
                 self.valid = tk.Label(self, text="This a valid credit card", bg='orange')
                 self.valid.pack()
+                
+                
+                
+                if self.UserInput.get()[0] == "1":
+                    self.cardType = tk.Label(self, text="Airlines", bg='orange')
+                if self.UserInput.get()[0] == "2":
+                    self.cardType = tk.Label(self, text="Airlines & Financial", bg='orange')
+                if self.UserInput.get()[0] == "3":
+                    print("American Express")
+                    self.cardType = tk.Label(self, text="American Express", bg='orange')
+                if self.UserInput.get()[0] == "4":
+                    print("Visa")
+                    self.cardType = tk.Label(self, text="Visa", bg='orange')
+                if self.UserInput.get()[0] == "5":
+                    print("Master Card")
+                    self.cardType = tk.Label(self, text="Master Card", bg='orange')
+                if self.UserInput.get()[0] == "6":
+                    print("Discover")
+                    self.cardType = tk.Label(self, text="Discover", bg='orange')
+                if self.UserInput.get()[0] == "7":
+                    print("Petroleum")
+                    self.cardType = tk.Label(self, text="Petroleum", bg='orange')
+                if self.UserInput.get()[0] == "8":
+                    print("Health Care & Telecommunications")
+                    self.cardType = tk.Label(self, text="Health Care & Telecommunications", bg='orange')
+                if self.UserInput.get()[0] == "9":
+                    print("Open for Assignment")
+                    self.cardType = tk.Label(self, text="Open for Assignment", bg='orange')
+                self.cardType.pack()
                 #print('This a valid credit card') 
 
+                
+                
+                
+                
             else:
                 self.retry2 = tk.Button(self, text="Try again?", command=self.Retry2, bg='orange')
                 self.retry2.pack()
@@ -68,3 +102,6 @@ class SampleApp(tk.Tk):
         #print(self.UserInput.get())        
 app = SampleApp()
 app.mainloop()
+#4276983748842032
+#6011356599462496
+#3448482730607508
